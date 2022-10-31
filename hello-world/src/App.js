5db1c5f61ad1f31d77beec85e846f52ca7362cb2
amin-leon <leonndayishimiye10@gmail.com>
2022-10-31 09:59:50 +0200

    hello world

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world, this is my first app in React Js 
         </p>
         <a
           className="App-link"
