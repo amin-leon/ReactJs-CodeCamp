f70b64ba464eb438bfdc68c8cfadf67cdad22bfd
amin-leon <104311071+amin-leon@users.noreply.github.com>
2022-11-03 09:36:37 +0200

    composing

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 //import Greet from './components/Gtreet';
 import Message from './components/Message';
+import All from './components/Composing';
+
+
 // import WithoutJsx from './components/Without-Jsx';
 function App() {
   return (
     <div className="App">
       <Message />
-
       {/* <Greet name="Leon"  heroName='Brue Lee'>
         <p>You are Dev</p>
         </Greet>
@@ -22,7 +24,6 @@ function App() {
       <Welcome name="Ab" heroName='Chan'/>  */}
       {/* <WithoutJsx /> */}
     </div>
-  );
+  )
 }
-
 export default App;
---
+++ hello-world/src/components/Composing.js
@@ -0,0 +1,40 @@
+// COMPOSING COMPONENTS IN ONE COMPONENT
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+// Navbar Component
+const Navbar=()=>{
+    return (
+    <h1>This is Navbar</h1>
+    )
+}
+ 
+// Sidebar Component
+const Sidebar=()=> {
+    return <h1>This is Sidebar.</h1>
+}
+ 
+// Article list Component
+const ArticleList=()=>
+{
+    return <h1>This is Articles List.</h1>
+}
+ 
+// App Component
+const All=()=>
+{
+    return(
+            <div>
+                <Navbar />
+                <Sidebar />
+                <ArticleList />
+            </div>
+        );
+}
+ 
+ReactDOM.render(
+    <All />,
+    document.getElementById("root")
+);
+
+export default All;
